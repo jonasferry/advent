9e5eb39e3255be6ba75fc4d17cbb63d542c7e775
Jonas Ferry <jonas.ferry@gmail.com>
2015-12-15 15:31:33 +0100

    Updated solution 2 part 1

--- 2.go
+++ 2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"sort"
@@ -8,21 +9,61 @@ import (
 	"strings"
 )
 
+func first(bxs []string) {
+	allwp := 0 // total wrapping paper
+
+	for _, bx := range bxs {
+
+		in := strings.Split(string(bx), "x") // in size
+
+		var ls [3]int // length sides
+
+		for i, v := range in {
+			ls[i], _ = strconv.Atoi(v) // convert to int
+		}
+
+		l, w, h := ls[0], ls[1], ls[2] // length, width, height
+
+		s := []int{2 * l * w, 2 * w * h, 2 * h * l}
+		sort.Ints(s)
+
+		wp := s[0] + s[0]/2 + s[1] + s[2] // wrapping paper
+
+		allwp += wp
+	}
+	fmt.Println("Total wrapping paper: " + strconv.Itoa(allwp))
+}
+
+func second(d []string) {
+}
+
 func main() {
-	in := strings.Split(os.Args[1], "x") // in size
-
-	var ls [3]int // length sides
-
-	for i, v := range in {
-		ls[i], _ = strconv.Atoi(v) // convert to int
+	var bxs []string
+	part, e := strconv.Atoi(os.Args[1]) //part 1 or 2
+	if e != nil {
+		panic(e)
 	}
 
-	l, w, h := ls[0], ls[1], ls[2] // length, width, height
+	f, e := os.Open(os.Args[2]) //open file
+	if e != nil {
+		panic(e)
+	}
+	defer f.Close()
 
-	s := []int{2 * l * w, 2 * w * h, 2 * h * l}
-	sort.Ints(s)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		bxs = append(bxs, scanner.Text())
+	}
 
-	wp := s[0] + s[0]/2 + s[1] + s[2] // wrapping paper
+	if e := scanner.Err(); e != nil {
+		panic(e)
+	}
 
-	fmt.Println(wp)
+	if part == 1 {
+		first(bxs)
+	} else if part == 2 {
+		second(bxs)
+	} else {
+		fmt.Println("Error: Select part 1 or 2")
+	}
 }
